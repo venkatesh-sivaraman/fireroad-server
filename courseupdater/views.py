cd190be00fe4ebd1e21e7cd2156e5263a0718620
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2017-12-25 22:05:08 -0500

    Added support for updating requirements files

--- courseupdater/views.py
+++ courseupdater/views.py
@@ -7,6 +7,7 @@ version_recursion_threshold = 100
 separator = "#,#"
 module_dir = os.path.dirname(__file__)  # get current directory
 global_file_names = ["departments", "enrollment"]
+requirements_dir = "requirements"
 
 def index(request):
     return HttpResponse("Hello, world. You're at the courseupdater index.")
@@ -16,10 +17,9 @@ def read_delta(url):
         ret = []
         lines = file.readlines()
         if len(lines) > 0:
-            season, year = lines.pop(0).split(separator)
-            ret.append((season, year))
+            ret.append(lines.pop(0).split(separator))
         else:
-            ret.append(("none", "none"))
+            ret.append([])
         if len(lines) > 0:
             version = lines.pop(0)
             ret.append(int(version))
@@ -33,6 +33,24 @@ def read_delta(url):
         ret.append(updated_files)
         return ret
 
+def compute_updated_files(version, base_dir):
+    updated_files = set()
+    version_num = version + 1
+    updated_version = version
+    while version_num < version_recursion_threshold:
+        url = os.path.join(base_dir, 'delta-{}.txt'.format(version_num))
+        if not os.path.exists(url):
+            print("Doesn't exist: {}".format(url))
+            break
+        semester, version, delta = read_delta(url)
+        if version != version_num:
+            print("Wrong version number in {}".format(url))
+        updated_files.update(set(delta))
+        updated_version = version_num
+        version_num += 1
+    return updated_files, updated_version
+
+
 '''Return an HTTP Response indicating the static file URLs that need to be
 downloaded. Every version of the course static file database will include a
 'delta-x.txt' file that specifies the semester, the version number, and the file
@@ -45,25 +63,13 @@ def check(request):
         return HttpResponseBadRequest('<h1>Invalid number of semester components</h1><br/><p>{}</p>'.format(semester))
     version = request.GET.get('v', '')
     try:
-        version_num = int(version) + 1
+        version_num = int(version)
     except ValueError:
         return HttpResponseBadRequest('<h1>Invalid version</h1><br/><p>{}</p>'.format(version))
 
     # Walk through the delta files
-    updated_files = set()
-    updated_version = version_num - 1
     semester_dir = comps[0] + '-' + comps[1]
-    while version_num < version_recursion_threshold:
-        url = os.path.join(module_dir, semester_dir, 'delta-{}.txt'.format(version_num))
-        if not os.path.exists(url):
-            print("Doesn't exist: {}".format(url))
-            break
-        semester, version, delta = read_delta(url)
-        if version != version_num:
-            print("Wrong version number in {}".format(url))
-        updated_files.update(set(delta))
-        updated_version = version_num
-        version_num += 1
+    updated_files, updated_version = compute_updated_files(version_num, os.path.join(module_dir, semester_dir))
 
     # Write out the updated files to JSON
     def url_comp(x):
@@ -72,4 +78,17 @@ def check(request):
         return semester_dir + '/' + x + '.txt'
     urls_to_update = list(map(url_comp, sorted(list(updated_files))))
     resp = {'v': updated_version, 'delta': urls_to_update}
+
+    # Check requirements also, if necessary
+    req_version = request.GET.get('rv', '')
+    if len(req_version) > 0:
+        try:
+            req_version_num = int(req_version)
+        except ValueError:
+            return HttpResponseBadRequest('<h1>Invalid requirements version</h1><br/><p>{}</p>'.format(req_version))
+        updated_files, updated_version = compute_updated_files(req_version_num, os.path.join(module_dir, requirements_dir))
+        urls_to_update = list(map(lambda x: requirements_dir + '/' + x + '.reql', sorted(list(updated_files))))
+        resp['rv'] = updated_version
+        resp['r_delta'] = urls_to_update
+
     return HttpResponse(json.dumps(resp), content_type="application/json")
