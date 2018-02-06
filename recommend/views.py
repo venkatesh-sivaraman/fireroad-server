b25a859920dfe280ff2cfd24986d7176006e6e6c
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2018-02-06 12:14:31 -0500

    Further request checking protocols

--- recommend/views.py
+++ recommend/views.py
@@ -62,28 +62,38 @@ def rate(request):
     if len(batch) > 0:
         batch_items = json.loads(batch)
         for item in batch_items:
-            if int(item['u']) == None:
-                return HttpResponseBadRequest('<h1>Missing user ID</h1>')
-            if item['s'] == None:
-                return HttpResponseBadRequest('<h1>Missing subject ID</h1>')
-            if int(item['v']) == None:
-                return HttpResponseBadRequest('<h1>Missing rating value</h1>')
-            if request.user.username != str(item['u']):
-                raise PermissionDenied
-            update_rating(int(item['u']), item['s'], int(item['v']))
+            try:
+                user_id = int(item['u'])
+                value = int(item['v'])
+                if user_id is None or user_id < 0:
+                    return HttpResponseBadRequest('<h1>Missing user ID</h1>')
+                if item['s'] == None or len(item['s']) > 10:
+                    return HttpResponseBadRequest('<h1>Missing subject ID</h1>')
+                if value is None:
+                    return HttpResponseBadRequest('<h1>Missing rating value</h1>')
+                if request.user.username != str(user_id):
+                    raise PermissionDenied
+                update_rating(user_id, item['s'], value)
+            except:
+                return HttpResponseBadRequest('<h1>Bad input</h1>')
         resp = { 'received': True }
     else:
         user_id = request.POST.get('u', '')
         subject_id = request.POST.get('s', '')
         value = request.POST.get('v', '')
-        if len(user_id) == 0 or int(user_id) < 0:
-            return HttpResponseBadRequest('<h1>Invalid user ID</h1><br/><p>{}</p>'.format(user_id))
-        if request.user.username != user_id:
-            raise PermissionDenied
-        if len(subject_id) == 0:
-            return HttpResponseBadRequest('<h1>Invalid subject ID</h1><br/><p>{}</p>'.format(subject_id))
-        if len(value) == 0 or int(value) > MAX_RATING_VALUE:
-            return HttpResponseBadRequest('<h1>Invalid rating value</h1><br/><p>{}</p>'.format(value))
-
-        resp = { 'u': int(user_id), 's': subject_id, 'v': int(value), 'received': True }
+        try:
+            user_id = int(user_id)
+            value = int(value)
+            if user_id is None or user_id < 0:
+                return HttpResponseBadRequest('<h1>Missing user ID</h1>')
+            if subject_id == None or len(subject_id) == 0 or len(subject_id) > 10:
+                return HttpResponseBadRequest('<h1>Missing subject ID</h1>')
+            if value is None:
+                return HttpResponseBadRequest('<h1>Missing rating value</h1>')
+            if request.user.username != str(user_id):
+                raise PermissionDenied
+            update_rating(user_id, subject_id, value)
+            resp = { 'u': user_id, 's': subject_id, 'v': value, 'received': True }
+        except:
+            return HttpResponseBadRequest('<h1>Bad input</h1>')
     return HttpResponse(json.dumps(resp), content_type="application/json")
