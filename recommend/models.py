262924f2ee830ffdc0353b1cc9538a928586ea5d
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2018-07-17 15:03:52 +0200

    Increased max road size; added some compressions for road JSON

--- recommend/models.py
+++ recommend/models.py
@@ -48,13 +48,16 @@ road_compressions = {
 class Road(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    contents = models.CharField(max_length=5000)
+    contents = models.CharField(max_length=10000)
 
     def __str__(self):
         return "{}: {}".format(self.user.username, self.contents)
 
     @staticmethod
     def compress_road(road_text):
+        road_text = road_text.replace("\n", "")
+        road_text = road_text.replace("\t", "")
+        road_text = road_text.replace('" : ', '":')
         for expr, sub in road_compressions:
             road_text = road_text.replace(expr, sub)
         return road_text
