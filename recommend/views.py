c9f28482a6f3b9acd8610af178f3111962fe7576
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2018-08-03 19:48:40 -0400

    Use native JSON object for road contents

--- recommend/views.py
+++ recommend/views.py
@@ -145,20 +145,20 @@ def upload_road(request):
     road_name = request.POST.get('name', '')
     if road_name is None or len(road_name) == 0:
         return HttpResponseBadRequest('<h1>Missing road name</h1>')
-    contents = request.POST.get('contents', '')
+    contents = request.POST.get('contents', {})
     if contents is None or len(contents) == 0:
         return HttpResponseBadRequest('<h1>Missing road contents</h1>')
 
     try:
-        j_contents = json.loads(contents)
+        j_contents = json.dumps(contents)
     except:
         return HttpResponseBadRequest('<h1>Invalid JSON for road contents</h1>')
     try:
         r = Road.objects.get(user=request.user, name=road_name)
-        r.contents = Road.compress_road(contents)
+        r.contents = Road.compress_road(j_contents)
         r.save()
     except:
-        r = Road(user=request.user, name=road_name, contents=Road.compress_road(contents))
+        r = Road(user=request.user, name=road_name, contents=Road.compress_road(j_contents))
         r.save()
     resp = { 'received': True }
     return HttpResponse(json.dumps(resp), content_type="application/json")
