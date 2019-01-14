8f5afa5bf738d1648e794c98d1a40ca8adba3b8d
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2019-01-14 16:42:53 -0500

    Renamed "catalog" endpoint to "courses" to disambiguate from "catalogs" URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'course/(?P<subject_id>[A-z0-9.]+)', views.lookup, name='lookup'),
+    url(r'lookup/(?P<subject_id>[A-z0-9.]+)', views.lookup, name='lookup'),
     url(r'dept/(?P<dept>[A-z0-9.]+)', views.department, name='department'),
     url(r'all', views.list_all, name='list_all')
 ]
