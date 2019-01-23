0eda559d9686ef18fa8591404e589f5efcf9b44d
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2019-01-23 15:22:33 -0500

    Add blank space for catalog base dir

--- fireroad/settings.py
+++ fireroad/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+CATALOG_BASE_DIR = ""
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
