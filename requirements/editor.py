f4d231cfbf4af893df5b5eddbea3870d3962aff7
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2019-04-27 12:47:58 -0400

    Moved diff logic to another file

--- requirements/editor.py
+++ requirements/editor.py
@@ -18,7 +18,7 @@ import logging
 from reqlist import *
 from views import REQUIREMENTS_EXT
 from django.http import Http404
-import numpy as np
+from .diff import build_diff
 
 NEW_DOC_ID = "new_doc"
 NEW_DOC_NAME = "new requirements list"
@@ -270,113 +270,6 @@ def build_presentation_items(list):
     return "\n".join(ret)
 
 # Review and commit (admin only)
-
-def best_diff_sequence(old, new, allow_subs=True):
-    """Compute the best diff sequence between the old and new requirements list
-    contents. Uses a simple DP-based edit distance algorithm. If allow subs is
-    True, the diff sequence may use 0 to denote either substitutions or the same
-    value."""
-
-    memo = np.zeros((len(old) + 1, len(new) + 1))
-
-    parent_pointers = np.zeros((len(old) + 1, len(new) + 1))
-    parent_pointers[-1,:] = 1
-    parent_pointers[:,-1] = -1
-    parent_pointers[-1,-1] = 0
-
-    for i in reversed(range(len(old) + 1)):
-        for j in reversed(range(len(new) + 1)):
-            if i == len(old) and j == len(new): continue
-
-            options = [] # Format: (score, label)
-            if i < len(old) and j < len(new) and (allow_subs or old[i] == new[j]):
-                options.append((memo[i+1, j+1] + (max(len(old[i]), len(new[j])) if old[i] != new[j] else 0), 0))
-            if i < len(old):
-                options.append((memo[i+1, j] + len(old[i]) + 1, -1))
-            if j < len(new):
-                options.append((memo[i, j+1] + len(new[j]) + 1, 1))
-
-            score, label = min(enumerate(options), key=lambda x: (x[1], x[0]))[1]
-            memo[i,j] = score
-            parent_pointers[i,j] = label
-
-    best_sequence = []
-    i = 0
-    j = 0
-    while i <= len(old) and j <= len(new):
-        best_sequence.append(parent_pointers[i,j])
-        if best_sequence[-1] == 0:
-            i += 1
-            j += 1
-        elif best_sequence[-1] == 1:
-            j += 1
-        elif best_sequence[-1] == -1:
-            i += 1
-    return best_sequence[:-1]
-
-WORD_FINDER_REGEX = r"[\w'.-]+|[^\w'.-]"
-
-def build_diff_line(old, new):
-    """Builds a single line of the diff."""
-    result = "<p class=\"diff-line\">"
-
-    if old == new:
-        result += old
-    else:
-        old_words = re.findall(WORD_FINDER_REGEX, old)
-        new_words = re.findall(WORD_FINDER_REGEX, new)
-        diff_sequence = best_diff_sequence(old_words, new_words, allow_subs=False)
-        i = 0
-        j = 0
-        current_change = None
-        for change in diff_sequence:
-            if change == 0: # Same character (since allow subs is False)
-                if current_change is not None:
-                    result += "</span>"
-                    current_change = None
-                result += escape(old_words[i])
-                i += 1
-                j += 1
-            elif change == 1: # Insertion
-                if current_change != 1:
-                    if current_change is not None:
-                        result += "</span>"
-                    result += "<span class=\"insertion\">"
-                    current_change = 1
-                result += escape(new_words[j])
-                j += 1
-            elif change == -1: # Deletion
-                if current_change != -1:
-                    if current_change is not None:
-                        result += "</span>"
-                    result += "<span class=\"deletion\">"
-                    current_change = -1
-                result += escape(old_words[i])
-                i += 1
-        if current_change is not None:
-            result += "</span>"
-    result += "</p>"
-    return result
-
-def build_diff(old, new):
-    old_lines = re.split(r'\r?\n', old)
-    new_lines = re.split(r'\r?\n', new + '\n')
-    diff_sequence = best_diff_sequence(old_lines, new_lines)
-    result = ""
-    i = 0
-    j = 0
-    for change in diff_sequence:
-        if change == 0: # Same character (since allow subs is False)
-            result += build_diff_line(old_lines[i], new_lines[j])
-            i += 1
-            j += 1
-        elif change == 1: # Insertion
-            result += "<p class=\"diff-line\"><span class=\"insertion\">" + escape(new_lines[j]) + "</span></p>"
-            j += 1
-        elif change == -1: # Deletion
-            result += "<p class=\"diff-line\"><span class=\"deletion\">" + escape(old_lines[i]) + "</span></p>"
-            i += 1
-    return result
 
 @staff_member_required
 def review(request, edit_req):
