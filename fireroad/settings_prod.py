52cb552c5200c34887f0d75278eae7530431f4d0
Venkatesh Sivaraman <venkats@mit.edu>
2019-10-10 00:28:09 -0400

    Added dev and prod settings files

---
+++ fireroad/settings_prod.py
@@ -0,0 +1,33 @@
+"""
+Django settings for fireroad project in prod environment only.
+"""
+
+import os
+from .settings import *
+
+# Location of catalog files on the server
+CATALOG_BASE_DIR = "/var/www/html/catalogs"
+
+# Security settings
+RESTRICT_AUTH_REDIRECTS = True
+DEBUG = False
+
+# For building URLs and validating hosts 
+ALLOWED_HOSTS = ['fireroad.mit.edu']
+MY_BASE_URL = 'https://fireroad.mit.edu'
+
+# MySQL database
+import dbcreds
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': dbcreds.dbname, # For scripts: venkats+fireroad
+        'USER': dbcreds.username,
+        'PASSWORD': dbcreds.password,
+        'HOST': dbcreds.host, # For scripts: sql.mit.edu
+        'PORT': '3306',
+    }
+}
+
+
