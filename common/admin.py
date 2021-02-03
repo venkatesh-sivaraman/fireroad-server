6ac6b9b6ff804f667086998e9394fdbabfab1f94
Venkatesh Sivaraman <venkats@mit.edu>
2021-02-02 19:03:01 -0500

    Remove approved_users field from APIClient admin

--- common/admin.py
+++ common/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+# The approved_users field creates some issues when trying to update APIClients,
+# so just remove it entirely
+class APIClientAdmin(admin.ModelAdmin):
+    exclude = ('approved_users',)
+
 admin.site.register(Student)
 admin.site.register(RedirectURL)
-admin.site.register(APIClient)
+admin.site.register(APIClient, APIClientAdmin)
